2e079507161ee8326ebd35b0d2eece6f68f04b6b
rosannamanninen <>
2023-01-12 17:02:06 +0200

    aloita seuraava

--- pohja/src/App.js
+++ pohja/src/App.js
@@ -1,60 +1,47 @@
-
-import Course from './components/course.jsx'
-
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
     }
-  ]
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
 
   return (
     <div>
-      {courses.map(
-        course => <Course course={course} key={course.id} />
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(
+        person => 
+        <p key={person.name}>
+          {person.name}
+        </p>
         )}
     </div>
   )
+
 }
 
 export default App
